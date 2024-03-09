7da5fd891eed4981fa54eb6e983e01595ae19f25
cire3 <cire3@gmail.com>
2024-03-09 16:04:57 -0600

    mixmilk solution

--- src/me/cire3/drafts/MixingMilk.java
+++ src/me/cire3/drafts/MixingMilk.java
@@ -2,8 +2,34 @@ package me.cire3.drafts;
 
 import me.cire3.Kattio;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class MixingMilk {
-    public static void begin() {
-        Kattio io = new Kattio(""
+    private static final int[] capacities = new int[3];
+    private static final int[] currentMilkLevel = new int[3];
+
+    public static void begin() throws IOException {
+        Kattio io = new Kattio("problems/mixingmilk/mixmilk");
+
+        for (int i = 0; i < 3; i++) {
+            capacities[i] = io.nextInt();
+            currentMilkLevel[i] = io.nextInt();
+        }
+
+        for (int i = 0; i < 100; i++) {
+            int fillingFrom = i % 3;
+            int fillingTo = (i + 1) % 3;
+
+            int leftToFill = capacities[fillingTo] - currentMilkLevel[fillingTo];
+
+            int amtShifted = Math.min(leftToFill, currentMilkLevel[fillingFrom]);
+
+            currentMilkLevel[fillingTo] += amtShifted;
+            currentMilkLevel[fillingFrom] -= amtShifted;
+        }
+
+        for (int milk : currentMilkLevel) io.println(milk);
+        io.close();
     }
 }
--- src/me/cire3/finalprograms/MixingMilkBronze2018DecemberProblem1.java
+++ src/me/cire3/finalprograms/MixingMilkBronze2018DecemberProblem1.java
@@ -1,4 +1,61 @@
 package me.cire3.finalprograms;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class MixingMilkBronze2018DecemberProblem1 {
+    private static final int[] capacities = new int[3];
+    private static final int[] currentMilkLevel = new int[3];
+
+    public static void main(String[] args) throws IOException {
+        Kattio io = new Kattio("problems/mixingmilk/mixmilk");
+
+        for (int i = 0; i < 3; i++) {
+            capacities[i] = io.nextInt();
+            currentMilkLevel[i] = io.nextInt();
+        }
+
+        for (int i = 0; i < 100; i++) {
+            int fillingFrom = i % 3;
+            int fillingTo = (i + 1) % 3;
+
+            int leftToFill = capacities[fillingTo] - currentMilkLevel[fillingTo];
+
+            int amtShifted = Math.min(leftToFill, currentMilkLevel[fillingFrom]);
+
+            currentMilkLevel[fillingTo] += amtShifted;
+            currentMilkLevel[fillingFrom] -= amtShifted;
+        }
+
+        for (int milk : currentMilkLevel) io.println(milk);
+        io.close();
+    }
+
+    private static class Kattio extends PrintWriter {
+        private final BufferedReader r;
+        private StringTokenizer st;
+        // standard input
+        public Kattio() { this(System.in, System.out); }
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+        // USACO-style file input
+        public Kattio(String problemName) throws IOException {
+            super(problemName + ".out");
+            r = new BufferedReader(new FileReader(problemName + ".in"));
+        }
+        // returns null if no more input
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(r.readLine());
+                return st.nextToken();
+            } catch (Exception ignored) { }
+            return null;
+        }
+        public int nextInt() { return Integer.parseInt(next()); }
+        public double nextDouble() { return Double.parseDouble(next()); }
+        public long nextLong() { return Long.parseLong(next()); }
+    }
 }
