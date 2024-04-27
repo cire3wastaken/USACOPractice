9cf43b649d7e0226ec2e7ac066cb8f3eee3e0e09
cire3 <cire3mc@gmail.com>
2024-04-27 08:06:08 -0500

    e

--- src/me/cire3/drafts/usaco2024/january/bronze/finalized/BalancingBacteriaUsacoJan2024BronzeProblem3.java
+++ src/me/cire3/drafts/usaco2024/january/bronze/finalized/BalancingBacteriaUsacoJan2024BronzeProblem3.java
@@ -1,17 +1,17 @@
-package me.cire3.drafts.usaco2024.january.bronze;
+package me.cire3.drafts.usaco2024.january.bronze.finalized;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// solution is O(n^2)
-public class BalancingBacteria {
-    public static void begin() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(("5\n" +
-                "1 3 -2 -7 5").getBytes())));
+public class BalancingBacteriaUsacoJan2024BronzeProblem3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        long operations = 0;
         long sum = 0;
         long diff = 0;
         long[] originals = new long[n];
