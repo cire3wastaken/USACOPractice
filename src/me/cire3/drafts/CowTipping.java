539804be3b6764c413a86cf062585b1f8940b4ff
cire3 <cire3mc@gmail.com>
2024-04-28 20:46:28 -0500

    begin cow tipping

--- src/me/cire3/drafts/CowTipping.java
+++ src/me/cire3/drafts/CowTipping.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class CowTipping {
@@ -18,13 +19,25 @@ public class CowTipping {
         boolean[][] tipped = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            char[] str = br.readLine().toCharArray();
             for (int j = 0; j < n; j++)
-                tipped[i][j] = Integer.parseInt(st.nextToken()) == 1;
+                tipped[i][j] = Integer.parseInt(String.valueOf(str[j])) == 1;
         }
 
+        int flips = 0;
         for (int i = n - 1; i >= 0; i--) {
-
+            for (int j = n - 1; j >= 0; j--) {
+                if (tipped[i][j]) {
+                    for (int k = 0; k < i; k++) {
+                        for (int l = 0; l < j; l++) {
+                            tipped[k][l] = !tipped[k][l];
+                        }
+                    }
+                    flips++;
+                }
+            }
         }
+
+        System.out.println(flips);
     }
 }
