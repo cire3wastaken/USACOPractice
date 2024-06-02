56f051cfe7b4ba6c8a9e686cf8d3a7a2af5700f9
cire3 <133830840+cire3wastaken@users.noreply.github.com>
2024-06-02 16:49:27 -0500

    add stress test to GrassPlanting

--- src/me/cire3/drafts/GrassPlanting.java
+++ src/me/cire3/drafts/GrassPlanting.java
@@ -1,10 +1,9 @@
 package me.cire3.drafts;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 @SuppressWarnings("rawtypes")
@@ -13,14 +12,14 @@ public class GrassPlanting {
     static int[] types;
     static List<Integer>[] neighbors;
     public static void begin() throws IOException {
-        System.out.println(beginImpl());
-    }
-
-    private static String beginImpl() throws IOException {
-        BufferedReader br = new BufferedReader(new StringReader("4\n" +
+        System.out.println(beginImpl("4\n" +
                 "1 2\n" +
                 "4 3\n" +
                 "2 3"));
+    }
+
+    private static String beginImpl(String testcase) throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader(testcase));
 
         int n = Integer.parseInt(br.readLine());
 
@@ -67,5 +66,44 @@ public class GrassPlanting {
 
             type++;
         }
+    }
+
+    public static void stressTest() throws IOException {
+        String testcase = "";
+
+        try {
+            for (int m = 0; m < 100000; m++) {
+                testcase = "";
+                Random random = new Random();
+
+                int n = random.nextInt(1001);
+                testcase += n;
+
+                for (int i = 1; i < n; i++) {
+                    testcase += "\n";
+                    int random1 = random.nextInt(n);
+                    int random2 = random.nextInt(n);
+
+                    if (random1 == random2) {
+                        if (random2 < n - 1)
+                            random2++;
+                        else
+                            random2--;
+                    }
+
+                    testcase += random1 + " " + random2;
+                }
+
+                beginImpl(testcase);
+            }
+        } catch (Exception e) {
+            System.out.println("Failed Testcase:");
+            System.out.println(testcase);
+            PrintWriter pw = new PrintWriter("failed_testcase.txt");
+            pw.println(testcase);
+            pw.close();
+            return;
+        }
+        System.out.println("Passed all test cases");
     }
 }
