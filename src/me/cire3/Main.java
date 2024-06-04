48d9f0794c3e6c0572bcd1a0ec4a57c958f9e557
cire3 <cire3mc@gmail.com>
2024-06-04 15:04:51 -0500

    Call TheGreatRevegation in main class

--- src/me/cire3/Main.java
+++ src/me/cire3/Main.java
@@ -6,6 +6,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        GrassPlanting.stressTest();
+        TheGreatRevegetation.begin();
     }
 }
