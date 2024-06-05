be4568482475075a720fe353b186c77812dc7fd8
cire3 <cire3mc@gmail.com>
2024-06-05 18:51:12 -0500

    make code more readable & intuitive

--- src/me/cire3/drafts/MilkFactory.java
+++ src/me/cire3/drafts/MilkFactory.java
@@ -22,17 +22,17 @@ public class MilkFactory {
 
         br.close();
 
-        int index = -2;
+        int index = -1;
         for (int i = 0; i < n; i++) {
             if (outgoingPaths[i] == 0) {
-                if (index != -2) {
-                    index = -2;
+                if (index != -1) {
+                    index = -1;
                     break;
                 }
                 index = i;
             }
         }
 
-        System.out.println(index + 1);
+        System.out.println(index == -1 ? -1 : index + 1);
     }
 }
