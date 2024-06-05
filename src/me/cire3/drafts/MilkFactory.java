c03dea20f2dc32fb949af6e5b66f4ad8cf89978d
cire3 <cire3mc@gmail.com>
2024-06-05 18:31:39 -0500

    begin parsing input for MilkFactory

--- src/me/cire3/drafts/MilkFactory.java
+++ src/me/cire3/drafts/MilkFactory.java
@@ -1,9 +1,22 @@
 package me.cire3.drafts;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
 
 public class MilkFactory {
     public static final synchronized strictfp void begin() throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader("3\n" +
+                "1 2\n" +
+                "3 2"));
 
+        int n = Integer.parseInt(br.readLine());
+
+    }
+
+    private static class TreeNode {
+        public TreeNode parent;
+        public List<TreeNode> children;
     }
 }
