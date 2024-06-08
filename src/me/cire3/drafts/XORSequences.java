0a01a83ad53861daef09f2d8fc06b76c648e05d6
cire3 <cire3mc@gmail.com>
2024-06-08 14:47:56 -0500

    begin parsing input for XOR Sequences

--- src/me/cire3/drafts/XORSequences.java
+++ src/me/cire3/drafts/XORSequences.java
@@ -1,9 +1,16 @@
 package me.cire3.drafts;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 
 public class XORSequences {
     public static void begin() throws IOException {
-
+        BufferedReader br = new BufferedReader(new StringReader("4\n" +
+                "0 1\n" +
+                "12 4\n" +
+                "57 37\n" +
+                "316560849 14570961\n"));
+        int t = Integer.parseInt(br.readLine());
     }
 }
