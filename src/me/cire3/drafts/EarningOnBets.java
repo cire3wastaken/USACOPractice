08556f8f97b9c05f7cf00bb93ad9c26c11e20454
cire3 <cire3mc@gmail.com>
2024-06-09 17:32:04 -0500

    setup input reading

--- src/me/cire3/drafts/EarningOnBets.java
+++ src/me/cire3/drafts/EarningOnBets.java
@@ -1,9 +1,23 @@
 package me.cire3.drafts;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
 
 public class EarningOnBets {
     public static void begin() throws IOException {
-
+        BufferedReader br = new BufferedReader(new StringReader("6\n" +
+                "3\n" +
+                "3 2 7\n" +
+                "2\n" +
+                "3 3\n" +
+                "5\n" +
+                "5 5 5 5 5\n" +
+                "6\n" +
+                "7 9 3 17 9 13\n" +
+                "3\n" +
+                "6 3 2\n" +
+                "5\n" +
+                "9 4 6 8 3\n"));
     }
 }
