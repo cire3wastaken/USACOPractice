472158032347b588ddd754647e8f8575b9eb82ef
cire3 <cire3mc@gmail.com>
2024-06-09 19:18:25 -0500

    add mainmethod oops

--- src/me/cire3/finalprograms/programs/EarningOnBetsCfProblemC.java
+++ src/me/cire3/finalprograms/programs/EarningOnBetsCfProblemC.java
@@ -1,12 +1,12 @@
-package me.cire3.finalprograms.programs;
-
+//package me.cire3.finalprograms.programs;
+//
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class EarningOnBetsCfProblemC {
-    public static void begin() throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(br.readLine());
