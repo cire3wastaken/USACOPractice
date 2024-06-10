29ddc481c5f82c83baf8e11784031cd9d63f8962
cire3 <cire3mc@gmail.com>
2024-06-10 14:59:07 -0500

    oops

--- src/me/cire3/drafts/EarningOnBets.java
+++ src/me/cire3/drafts/EarningOnBets.java
@@ -39,8 +39,10 @@ public class EarningOnBets {
             for (int j : arr)
                 a += 1 / j;
 
-            if (a >= 1)
+            if (a >= 1) {
                 System.out.println(-1);
+                continue;
+            }
 
             for (int j = 0; j < n; j++)
                 System.out.print(lcm / arr[j] + " ");
